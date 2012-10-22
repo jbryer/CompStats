1d15582b0658f3e9d30337e50b0bc7a552286bfe
Jason Bryer <jason@bryer.org>
2012-10-22 11:18:34 -0400

    Initial version.

---
+++ Installation/Setup.r
@@ -0,0 +1,33 @@
+pkgs <- c('Deducer', 'devtools', 'doBy', 'foreign', 'gdata', 'ggplot2',
+          'gmaps', 'Hmisc', 'JGR', 'knitr', 'maps', 'mapdata', 'mapprov',
+          'maptools', 'psych', 'R2wd', 'RCurl', 'reshape', 'RODBC',
+          'roxygen2', 'seqinr', 'sm', 'sp', 'sqldf', 'survey', 'WriteXLS',
+          'XML', 'xtable')
+
+.libPaths()[1] #This is where R will install the packages
+
+#This will install the list of packages above. Good for a new installation
+is_installed <- function(mypkg) { is.element(mypkg, installed.packages()[,1]) }
+for(l in pkgs) {
+	if(!is_installed(l)) {
+		install.packages(l, dependencies=TRUE,
+			repos=c("http://www.stats.ox.ac.uk/pub/RWin", "http://cran.r-project.org"))
+	}
+}
+
+#If you already have R installed, this function will look for updates to any
+#installed packages.
+update.packages(ask=FALSE)
+
+#The following packages will be installed from github.com. The install_github
+#function is in the devtools package.
+require(devtools)
+install_github('retention','jbryer')
+install_github('irutils','jbryer')
+install_github('ipeds','jbryer')
+install_github('makeR','jbryer')
+install_github('pisa','jbryer')
+install_github('multilevelPSA','jbryer')
+
+library() #Return list of installed packages
+search() #Return list of loaded packages
